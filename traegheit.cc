39a081e0862af39535b5b87e1986c34ba6499a5a
Bennet <103441059+Bennetfhh@users.noreply.github.com>
2022-06-20 14:41:34 +0000

    abcd e/2

--- traegheit.cc
+++ traegheit.cc
@@ -3,6 +3,7 @@
 #include "Vollzylinder.hh"
 #include <iostream>
 #include <memory>
+#include <cmath>
 
 int main() {
   const int N = 10000;     // Anzahl Integrationspunkte
@@ -18,14 +19,14 @@ int main() {
   std::cin >> u;
 
   std::unique_ptr<Zylindermantel> zm(new Zylindermantel(ZM_R, ZM_L));
-
-  double J = 0;     // Massentraegheitsmoment
+  double J = 0;    // Massentraegheitsmoment
   double m = M / N; // Masse eines Massenpunktes
   for (int i = 0; i < N; ++i) {
     Vektor x = zm->punkt();
     // Abstand Punkt x und Gerade a + t*u
     // Vektor n = ...;//Normalenvektor x-a kreuz u
-    double r = 0; //|n|/|u|
+    Vektor n = (x - a).kreuz(u);
+    double r = n.betrag() / u.betrag(); //|n|/|u|
     // std::cout << x << " :" << r << std::endl;
     // addiere Beitrag des Massenpunktes zum Traegheitsmoment
     J += m * r * r;
