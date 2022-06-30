886d143ee486c94b14bf11d42e76490193221edd
Bennet <103441059+Bennetfhh@users.noreply.github.com>
2022-06-30 15:00:55 +0000

    d

--- Vollzylinder.cc
+++ Vollzylinder.cc
@@ -9,9 +9,9 @@ Vollzylinder::Vollzylinder(double nr, double nl) {
 }
 
 Vektor Vollzylinder::punkt() {
-  double R = 0;
-  double phi = 2 * M_PI * sqrt(rand())/ (double)RAND_MAX;
-  double z = l_ * (sqrt(rand()) / (double)RAND_MAX) - 0.5;
+  double R = r_ * sqrt(rand() / (double)RAND_MAX);
+  double phi = 2 * M_PI * rand() / (double)RAND_MAX;
+  double z = l_ * (rand() / (double)RAND_MAX - 0.5);
 
   double x = R * cos(phi);
   double y = R * sin(phi);
